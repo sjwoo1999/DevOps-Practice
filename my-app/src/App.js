5e667bc71d9d2ba03e65aa485e44ab56bfb7c4b8
_woo_s.j <91654658+sjwoo1999@users.noreply.github.com>
2023-11-21 21:49:21 +0900

    Update App.js text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -32,7 +32,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Text
         </a>
         <p>
             Visitors : {counter.counter}
