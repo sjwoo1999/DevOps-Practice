1ba7a134290b42f64d137178ff354dc99223c7f5
_woo_s.j <91654658+sjwoo1999@users.noreply.github.com>
2023-11-21 21:53:24 +0900

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -4,12 +4,20 @@ const port = 3002;
 
 const apiRouter = express.Router();
 
+var counter = 0;
+
 apiRouter.get('/', (req, res) => {
+	counter = counter + 1;
 	res.json({ message: 'Hello, world!'});
+});
+
+apiRouter.get('/counter', (req, res) => {
+	counter = counter + 1;
+	res.json({'counter': `${counter}`});
 });
 
 app.use('/myweb/api/v1', apiRouter);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
-})
+});
